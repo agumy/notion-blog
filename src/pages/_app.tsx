184f9c4c09b402598a5d0a39f5b376a8e00e7a2c
agumy <nov13sh@gmail.com>
2021-02-11 23:38:58 +0900

    fix global app apge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
+import Header from '../components/header'
 import Footer from '../components/footer'
+import appStyle from '../styles/app.module.css'
 
 export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-  </>
+  <div className={appStyle.app}>
+    <div className={appStyle.header}>
+      <Header />
+    </div>
+    <main className={appStyle.main}>
+      <Component {...pageProps} />
+    </main>
+    <div className={appStyle.footer}>
+      <Footer />
+    </div>
+  </div>
 )
