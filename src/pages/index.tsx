a81ff52dacdece28a8807777c5248c2d16f7122f
agumy <nov13sh@gmail.com>
2021-02-12 23:01:00 +0900

    add post component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-import Header from '../components/header'
 import homeStyles from '../styles/home.module.css'
+import Post from '../components/post'
 
-export default () => <></>
+export default function Home() {
+  return (
+    <div className={homeStyles.home}>
+      {[...Array(10).keys()].map(n => (
+        <Post />
+      ))}
+    </div>
+  )
+}
