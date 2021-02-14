179f79123c936206bf45722bc14f1c37bea3eb99
agumy <nov13sh@gmail.com>
2021-02-14 23:56:12 +0900

    fix style

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,12 +3,12 @@ import homeStyles from '../styles/home.module.css'
 export default function Post() {
   return (
     <div className={homeStyles.post}>
-      <span className={homeStyles.title}>
-        Slackで確認して欲しいことが流れていく…そんな時こそ、チームでのスプレッドシート活用例
-      </span>
       <div className={homeStyles.image}>
         <img src="https://designmemo.jp/wp-content/uploads/2020/03/eyecatch_check-sheet-600x400.jpg" />
       </div>
+      <span className={homeStyles.title}>
+        Slackで確認して欲しいことが流れていく…そんな時こそ、チームでのスプレッドシート活用例
+      </span>
     </div>
   )
 }
