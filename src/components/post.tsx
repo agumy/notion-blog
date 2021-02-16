36f529148947c6e5aaf5e87cc64a289dde5f141b
agumy <nov13sh@gmail.com>
2021-02-16 23:25:58 +0900

    improve style

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,14 +1,16 @@
 import homeStyles from '../styles/home.module.css'
 
-export default function Post() {
+export default function Post({ post }: { post: any }) {
+  console.log(post)
   return (
     <div className={homeStyles.post}>
       <div className={homeStyles.image}>
         <img src="https://designmemo.jp/wp-content/uploads/2020/03/eyecatch_check-sheet-600x400.jpg" />
       </div>
-      <span className={homeStyles.title}>
-        Slackで確認して欲しいことが流れていく…そんな時こそ、チームでのスプレッドシート活用例
-      </span>
+      <div className={homeStyles.page}>
+        <span className={homeStyles.title}>{post.Page}</span>
+        <span>{new Date(post.Date).toDateString()}</span>
+      </div>
     </div>
   )
 }
