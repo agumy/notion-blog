cc6b18865b6258a4602257e6f607afc72f3f28fb
agumy <nov13sh@gmail.com>
2021-02-16 23:25:37 +0900

    fetch posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,62 @@
 import homeStyles from '../styles/home.module.css'
 import Post from '../components/post'
 
-export default function Home() {
+import Link from 'next/link'
+import Header from '../components/header'
+
+import blogStyles from '../../styles/blog.module.css'
+import sharedStyles from '../../styles/shared.module.css'
+
+import { getBlogLink, getDateStr, postIsPublished } from '../lib/blog-helpers'
+import { textBlock } from '../lib/notion/renderers'
+import getNotionUsers from '../lib/notion/getNotionUsers'
+import getBlogIndex from '../lib/notion/getBlogIndex'
+
+type Props = {
+  preview: boolean
+  posts: any[]
+}
+
+export default function Home(props: Props) {
   return (
     <div className={homeStyles.home}>
-      {[...Array(5).keys()].map(n => (
-        <Post key={n} />
+      {props.posts.map(post => (
+        <Post key={post.id} post={post} />
       ))}
     </div>
   )
+}
+
+export async function getStaticProps({ preview }) {
+  const postsTable = await getBlogIndex()
+
+  const authorsToGet: Set<string> = new Set()
+  const posts: any[] = Object.keys(postsTable)
+    .map(slug => {
+      const post = postsTable[slug]
+      // remove draft posts in production
+      if (!preview && !postIsPublished(post)) {
+        return null
+      }
+      post.Authors = post.Authors || []
+      for (const author of post.Authors) {
+        authorsToGet.add(author)
+      }
+      return post
+    })
+    .filter(Boolean)
+
+  const { users } = await getNotionUsers([...authorsToGet])
+
+  posts.map(post => {
+    post.Authors = post.Authors.map(id => users[id].full_name)
+  })
+
+  return {
+    props: {
+      preview: preview || false,
+      posts,
+    },
+    unstable_revalidate: 10,
+  }
 }
